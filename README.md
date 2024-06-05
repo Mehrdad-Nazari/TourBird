0472403109fdaf2b13386218f9cc22f24804bbd3
Mehrdad Nazari <mehrdadnazari@Mehrdads-MacBook-Pro.fritz.box>
2024-06-05 16:21:59 +0200

    TourBird

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# TourBird
-UI Built using React and Material-UI
+# Tourbird
+
+Tourbird is a web application designed to showcase tours offered by a tour company. Built using React and Material-UI, Tourbird provides users with an intuitive interface to explore different tour packages, view tour details, and book their next adventure.
+
+[Click here to try demo!](https://6660722086974f2311829521--serene-frangipane-b92b98.netlify.app/)
+
+## Features
+
+- Explore a variety of tour packages offered by the tour company.
+- Click on a tour to view more details, including duration, rating, price, and reviews.
+- Easily book a tour by selecting the desired dates and completing the booking process.
+
+## Used Technologies
+
+- React: JavaScript library for building user interfaces.
+- Material-UI: React components for faster and easier web development.
+- React Router: Declarative routing for React applications.
+
+This project was bootstrapped with Create React App. Icons provided by Material-UI Icons.
---
+++ src/components/AppBar.js
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+
+import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: "auto",
+  marginRight: "auto",
+  width: "auto",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1, marginBottom:"80px" }}>
+      <AppBar>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              Tourbird
+            </Typography>
+          </Link>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
